360b8d15bbadc8831d801ce61458ad9d0ee83dab
patel <patel@LAPTOP-DI7UQJNU>
2023-02-08 18:58:56 -0500

    Add project files.

---
+++ Deliverable_4/Program.cs
@@ -0,0 +1,29 @@
+﻿/* Author: Khushbu Patel
+ * Date: 2/5/2023
+ * Description: A C# console app that computes the first 25 numbers in a fibonacci sequence
+ */
+
+using System;
+namespace Deliverable_4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] fibonacci = new int[25];
+
+            // Initialize fibonacci series
+            fibonacci[0] = 0;
+            fibonacci[1] = 1;
+
+            for (int i = 0; i < 25; i++)
+            {
+                if (i >= 2)
+                {
+                    fibonacci[i] = fibonacci[i - 1] + fibonacci[i - 2];
+                }
+                Console.WriteLine("Fibonacci(" + i + ") = " + fibonacci[i]);
+            }
+        }
+    }
+}
